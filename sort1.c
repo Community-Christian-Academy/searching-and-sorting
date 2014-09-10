738eaf7745c8f0065e391231624f763fdcd5650e
Braden Smith <lukkht@gmail.com>
2014-09-10 08:02:01 -0500

    added sort1.c
    i didn't have this file on my branch for some reason

---
+++ sort1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void sort(int x) {
+    printf("sorted to:  %d\n", x);
+}
+
+void sortTwoNumbers(int x, int y) {
+    if (x < y) {
+        printf("already in order as %d %d\n", x, y);
+    } else {
+        printf("numbers were out of order.  sorted to %d %d\n", y, x);
+    }
+}
+
+void otherSorter(int a, int b) {
+    int biggestNumber;
+    int smallestNumber;
+    
+    if (a < b) {
+        biggestNumber = b;
+        smallestNumber = a;
+    } else {
+        biggestNumber = a;
+        smallestNumber = b;
+    }
+    printf("sorted to %d %d\n", smallestNumber, biggestNumber);
+}
+
+void anotherSorter(int x, int y) {
+    printf("sorted to %d %d\n", x < y ? x : y, x < y ? y : x);
+}
+
+
+
+int main() {
+    sort(12);
+    anotherSorter(238476,245);
+}
