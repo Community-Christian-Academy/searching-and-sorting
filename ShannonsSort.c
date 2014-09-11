184cd924e7c971d45c292a4768add9dcf1072db7
Shannon Eckelkamp <shannoneckelkamp@Shannons-MacBook-Pro.local>
2014-09-10 21:08:14 -0500

    added some stuff

--- ShannonsSort.c
+++ ShannonsSort.c
@@ -59,6 +59,34 @@ void biggerSorter(int a, int b, int c) {
         Number = b;
         biggerNumber = c;
         biggestNumber = a;
+    } if ((a == b) && (b < c)) {
+        Number = a;
+        biggerNumber = b;
+        biggestNumber = c;
+    } if ((a == c) && (a < b)) {
+        Number = a;
+        biggerNumber = c;
+        biggestNumber = b;
+    } if ((b == c) && (c < a)) {
+        Number = b;
+        biggerNumber = c;
+        biggestNumber = a;
+    } if ((a == b) && (b == c)) {
+        Number = a;
+        biggerNumber = b;
+        biggestNumber = c;
+    } if ((a < b) && (b = c)) {
+        Number = a;
+        biggerNumber = b;
+        biggestNumber = c;
+    } if ((b < a) && (a = c)) {
+        Number = b;
+        biggerNumber = a;
+        biggestNumber = c;
+    } if ((c < a) && (a = b)) {
+        Number = c;
+        biggerNumber = a;
+        biggestNumber = b;
     }
     printf("totes quashed: %d %d %d\n", Number, biggerNumber, biggestNumber);
 }
@@ -66,5 +94,5 @@ void biggerSorter(int a, int b, int c) {
 int main() {
     sort(12);
     anotherSorter(238476,245);
-    biggerSorter(789, 285, 1);
+    biggerSorter(2, 10, 2);
 }
