c2e6146bde9f4948492151bb67eadc6abbdd2d80
J. Campbell <fork42@mac.com>
2014-09-12 08:16:44 -0500

    refactors biggestNumber

--- biggestNumber.c
+++ biggestNumber.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int biggerOf(int x, int y) {
+    if (x > y) {
+        return x;
+    }
+    return y;
+}
+
 int main(int argc, char** argv) {
     int biggestNumber = 0;
     printf("hey, there are %d numbers from which to choose\n", argc-1);
@@ -8,9 +15,7 @@ int main(int argc, char** argv) {
     int i;
     for (i = 1 ; i < argc; i++) {
         int thisNumber = atoi(argv[i]);
-        if (thisNumber > biggestNumber) {
-            biggestNumber = thisNumber;
-        }
+        biggestNumber = biggerOf(thisNumber, biggestNumber);
     }
     
     printf("the biggest number is %d\n", biggestNumber);
