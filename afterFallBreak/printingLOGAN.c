de3af3ecad944b05d9c501b0347ea0f3eb28bc12
Logan Browning <logan_80@comcast.net>
2014-10-29 08:30:21 -0500

    cleared out even more...
    made it to print name[i]

--- afterFallBreak/printingLOGAN.c
+++ afterFallBreak/printingLOGAN.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-//int lengthString(char *s) {
-//
-//    int i;
-//
-//    for (i = 0; i < 10000000; i++) {
-//        if (s[i] == 0) {
-//            return i;
-//        }
-//    }
-//}
+int lengthString(char *s) {
+
+    int i;
+
+    for (i = 0; i < 10000000; i++) {
+        if (s[i] == 0) {
+            return i;
+        }
+    }
+}
 
 int main() {
 
@@ -17,27 +17,10 @@ int main() {
 
     int i;
 
-    for (i = 0; i < 1; i++) {
+    for (i = 0; i < lengthString(name); i++) {
     
-	    if (name[0]) {
-                printf("%c %d\n", name[0], name[0]);
-
-	        if (name[1]) {
-                   printf("%c %d\n", name[1], name[1]);
-
-		    if (name[2]) {
-                       printf("%c %d\n", name[2], name[2]);
-
-		        if (name[3]) {
-                           printf("%c %d\n", name[3], name[3]);
-
-                            if (name[4]) {
-			        printf("%c %d\n", name[4], name[4]);
-
-                            }
-                        }
-                    }
-                 }
+	    if (name[i]) {
+                printf("%c %d\n", name[i], name[i]);
             }
     }
 }
