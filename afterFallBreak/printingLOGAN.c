6630c430a049c80476257f9fa67c444a5f6b7697
Logan Browning <logan_80@comcast.net>
2014-10-29 08:22:03 -0500

    cleaned up printingLOGAN.c

--- afterFallBreak/printingLOGAN.c
+++ afterFallBreak/printingLOGAN.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 
-int lengthString(char *s) {
-
-    int i;
-
-    for (i = 0; i < 10000000; i++) {
-        if (s[i] == 0) {
-            return i;
-        }
-    }
-}
+//int lengthString(char *s) {
+//
+//    int i;
+//
+//    for (i = 0; i < 10000000; i++) {
+//        if (s[i] == 0) {
+//            return i;
+//        }
+//    }
+//}
 
 int main() {
 
-    char *theString = "LOGAN";
-    char *theSearch = "LOGAN";
+    char *name = "LOGAN";
 
     int i;
 
-    for (i = 0; i < lengthString(theString); i++) {
+    for (i = 0; i < 1; i++) {
     
-	    if (theSearch[0] == theString[i+0]) {
-                printf("%c %d\n", theSearch[0], theSearch[0]);
+	    if (name[0]) {
+                printf("%c %d\n", name[0], name[0]);
 
-	        if (theSearch[1] == theString[i+1]) {
-                   printf("%c %d\n", theSearch[1], theSearch[1]);
+	        if (name[1]) {
+                   printf("%c %d\n", name[1], name[1]);
 
-		    if (theSearch[2] == theString[i+2]) {
-                       printf("%c %d\n", theSearch[2], theSearch[2]);
+		    if (name[2]) {
+                       printf("%c %d\n", name[2], name[2]);
 
-		        if (theSearch[3] == theString[i+3]) {
-                           printf("%c %d\n", theSearch[3], theSearch[3]);
+		        if (name[3]) {
+                           printf("%c %d\n", name[3], name[3]);
 
-                            if (theSearch[4] == theString[i+4]) {
-			        printf("%c %d\n", theSearch[4], theSearch[4]);
+                            if (name[4]) {
+			        printf("%c %d\n", name[4], name[4]);
 
                             }
                         }
                     }
-                }
-           }
+                 }
+            }
     }
 }
